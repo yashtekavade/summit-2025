b112b8e0418d5a6b465830850793821f020de3b6
Devyani Sisodia <devu060704@gmail.com>
2025-01-14 22:52:39 +0530

    14th

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,44 +2,73 @@ import React from "react";
 import Footer from "../Components/Footer/Footer";
 import "./About.css";
 import a1 from "../assets/images/aboutgrp.webp";
-import videoPath from "../assets/images/mitwpusummit.mp4";
+import { FlipWords } from "../Components/ui/flip-words";
+// import { FloatingDock } from "../Components/ui/floating-dock";
+// import {
+//   IconHome,
+//   IconTerminal2,
+//   IconNewSection,
+//   IconBrandGithub,
+//   IconBrandX,
+// } from "@tabler/icons-react";
+// import { AnimatedTestimonials } from "../Components/ui/animated-testimonials";
 
 const About = () => {
-  const handleDownload = () => {
-    // Implement the brochure download functionality
-  };
+  const flipWords = ["unite", "inspire", "compete", "celebrate"];
+  // const dockItems = [
+  //   {
+  //     title: "Home",
+  //     icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+  //     href: "#",
+  //   },
+  //   {
+  //     title: "Products",
+  //     icon: <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+  //     href: "#",
+  //   },
+  //   {
+  //     title: "Components",
+  //     icon: <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+  //     href: "#",
+  //   },
+  //   {
+  //     title: "GitHub",
+  //     icon: <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+  //     href: "#",
+  //   },
+  //   {
+  //     title: "Twitter",
+  //     icon: <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+  //     href: "#",
+  //   },
+  // ];
+
 
   return (
     <div>
-      {/* Video Section */}
-      <div className="teaser">
-        <video src={videoPath} autoPlay loop muted />
-      </div>
-
-      {/* About Section */}
       <div className="about-div">
-        <h2 className="about-title">About MIT-WPU Summit 2024</h2>
+        <h2 className="about-title">
+          About MIT-WPU <FlipWords words={flipWords} /> Summit 2025.
+        </h2>
         <p className="about-subtitle">
-          India’s biggest Annual Inter-Engineering Sports event hosted by
-          MIT-World Peace University, committed towards the goal of ‘Nation
-          Building through Sports’.
+          India's biggest Annual Inter-Engineering Sports event hosted by
+          MIT-World Peace University committed towards the goal of 'Nation
+          Building through Sports'.
         </p>
       </div>
-      <button className="brochure-button" onClick={handleDownload}>
-    Brochure
-  </button>
-
-      {/* Image Section */}
       <div className="image-container">
         <img src={a1} alt="about" className="about-image" loading="lazy" />
       </div>
-      
-
+      {/* <div className="flex items-center justify-center h-[35rem] w-full mt-16">
+        <FloatingDock
+          mobileClassName="translate-y-20"
+          items={dockItems}
+        />
+      </div> */}
+    
       <Footer />
     </div>
   );
 };
 
 export default About;
-  
-
