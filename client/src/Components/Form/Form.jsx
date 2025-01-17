5a3e602e021e78b8a1be65baeb0be16da9e0357b
Saubhagya Singh <saubhagyasingh65@gmail.com>
2025-01-18 01:03:07 +0530

    fix 2

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -99,7 +99,7 @@ const Form = (props) => {
     try {
       // Make the POST request to the server URL
       const response = await axios.post(
-        "http://82.112.236.149:3300/summit/register",
+        "https://82.112.236.149:3300/summit/register",
         formDataWithImage
       );
 
